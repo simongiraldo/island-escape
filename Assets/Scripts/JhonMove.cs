2cbf2766e4c5ec3cdfa807ffbaebef2261563937
Santlas3 <112207581+Santlas3@users.noreply.github.com>
2022-10-24 15:43:46 -0500

    El vergas ya puede disparar en el segundo nivel 2.0

--- Assets/Scripts/JhonMove.cs
+++ Assets/Scripts/JhonMove.cs
@@ -9,13 +9,14 @@ public class JhonMove : MonoBehaviour
 
 public float Speed;
 public float JumpForce;
+public GameObject BulletPrefab;
 
 
-private Rigidbody2D Rigidbody2D;
-private float Horizontal;
-private bool Grounded;
-private Animator Animator;
-    
+    private Rigidbody2D Rigidbody2D;
+    private float Horizontal;
+    private bool Grounded;
+    private Animator Animator;
+    private float LastShoot;
     
     void Start()
     {
@@ -46,13 +47,34 @@ private Animator Animator;
         {
             Jump();
         }
+
+
+            if (Input.GetKey(KeyCode.Space) && Time.time > LastShoot + 0.25f)
+        {
+            Shoot();
+            LastShoot = Time.time; 
+        }
     }
+
 
     private void Jump()
     {
         Rigidbody2D.AddForce(Vector2.up * JumpForce);
     }
- 
+
+    
+    private void Shoot()
+    {
+        Vector3 direction;
+
+        if (transform.localScale.x == 10.80244f ) direction = Vector3.right;
+        else direction= Vector3.left;
+
+        GameObject Bullet = Instantiate(BulletPrefab, transform.position + direction * 0.1f , Quaternion.identity);
+        Bullet.GetComponent<BulletScrip>().SetDirection(direction);
+    }
+
+
     private void FixedUpdate()
     {
         Rigidbody2D.velocity = new Vector2(Horizontal * Speed , Rigidbody2D.velocity.y);
